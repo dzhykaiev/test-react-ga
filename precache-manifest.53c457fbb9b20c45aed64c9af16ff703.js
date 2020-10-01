4cb69f4851eaa73aca2232eb2b2104a7ed2eca25
dzhykaiev <yevhenii.dzhykaiev@gmail.com>
2020-10-02 00:11:34 +0300

    Updates

---
+++ precache-manifest.53c457fbb9b20c45aed64c9af16ff703.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6d9bf11a0ff740c53257d9f9297e54",
+    "url": "/test-react-ga/index.html"
+  },
+  {
+    "revision": "cbe1ca42ab00b6798f98",
+    "url": "/test-react-ga/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d12209f84382bd18bfc",
+    "url": "/test-react-ga/static/js/2.ca0f3a52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-react-ga/static/js/2.ca0f3a52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbe1ca42ab00b6798f98",
+    "url": "/test-react-ga/static/js/main.cc5eae6b.chunk.js"
+  },
+  {
+    "revision": "66524ae20977b3e5132c",
+    "url": "/test-react-ga/static/js/runtime-main.d010ca5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react-ga/static/media/logo.5d5d9eef.svg"
+  }
+]);
